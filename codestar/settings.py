0e519a50471a163fbb90d5c451a070441fac51f7
Helen Smith <5b64nnhi@students.codeinstitute.net>
2025-01-25 16:03:01 +0000

    FEAT; Install dj database adn psycopg2

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m56t3#m2yavs6=#3eqho!lb@#l1e1d87-16i#_5!z2r(57n^z$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-helsbelsdel-djangoblog-s2tvyhbunkz.ws.codeinstitute-ide.net',
 '.herokuapp.com']
@@ -75,11 +79,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#  }
+# }
+
+DATABASS = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
