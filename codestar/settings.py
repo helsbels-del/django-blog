d4a7911504debf497c0ad935ee94ce9cba2e40bd
Helen Smith <5b64nnhi@students.codeinstitute.net>
2025-01-25 16:55:04 +0000

    FIX: Add new SECRET_KEY

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m56t3#m2yavs6=#3eqho!lb@#l1e1d87-16i#_5!z2r(57n^z$'
+# SECRET_KEY = 'django-insecure-m56t3#m2yavs6=#3eqho!lb@#l1e1d87-16i#_5!z2r(57n^z$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
