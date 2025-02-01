dde311762140f0ef7fd43009e6c7f54482abdb83
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-01 08:12:28 +0000

    FEAT: Add env.py file

---
+++ env.py
@@ -0,0 +1,2 @@
+SECRET_KEY=gvp%(_f*wu6l)+_*c70x795&)!hsl26plt+-2&n(0=(9wl%rto
+DATABASE_URL=postgresql://neondb_owner:Pypi4Xq0bTVO@ep-dry-lake-a2hzfleu.eu-central-1.aws.neon.tech/royal_viral_gloss_383869
