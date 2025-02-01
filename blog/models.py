79868146b74d1052c142a6a82c1851699f3c6f6e
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-01 12:50:00 +0000

    FEAT: Add meta class to comments and add dunder

--- blog/models.py
+++ blog/models.py
@@ -33,4 +33,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}" 
+
     
