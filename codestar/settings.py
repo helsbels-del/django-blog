b5dbdb7df79ac4a7a430b705864a7e197f7d5151
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-09 12:21:43 +0000

    CHORE: Add missing comma

--- codestar/settings.py
+++ codestar/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
     'about',
