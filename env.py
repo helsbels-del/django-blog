b90b618819773db01951ca45d1f2df66d34dc57e
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-15 11:43:47 +0000

    FEAT: Add cloudinary

--- env.py
+++ env.py
@@ -5,3 +5,6 @@ os.environ.setdefault(
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:Pypi4Xq0bTVO@ep-dry-lake-a2hzfleu.eu-central-1.aws.neon.tech/royal_viral_gloss_383869")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@drmfroknk")
